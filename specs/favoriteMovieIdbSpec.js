423c3715e16d5ecc72b5fbf10b6eb318d6aa624a
cakrawalabahasa <officialcakrawalabahasa@gmail.com>
2021-07-05 04:56:46 +0700

    innitial commit

---
+++ specs/favoriteMovieIdbSpec.js
@@ -0,0 +1,55 @@
+import { itActsAsFavoriteMovieModel } from './contract/favoriteMovieContract';
+import FavoriteMovieIdb from '../src/scripts/data/favorite-movie-idb';
+ 
+describe('Favorite Movie Idb Contract Test Implementation', () => {
+  afterEach(async () => {
+    (await FavoriteMovieIdb.getAllMovies()).forEach(async (movie) => {
+      await FavoriteMovieIdb.deleteMovie(movie.id);
+    });
+  });
+ 
+  itActsAsFavoriteMovieModel(FavoriteMovieIdb);
+});
+
+     
+let favoriteMovies = [];
+ 
+const FavoriteMovieArray = {
+ 
+  getMovie(id) {
+    if (!id) {
+      return;
+    }
+ 
+    return favoriteMovies.find((movie) => movie.id == id);
+  },
+ 
+  getAllMovies() {
+    return favoriteMovies;
+  },
+ 
+  putMovie(movie) {
+    if (!movie.hasOwnProperty('id')) {
+      return;
+    }
+ 
+    // pastikan id ini belum ada dalam daftar favoriteMovies
+    if (this.getMovie(movie.id)) {
+      return;
+    }
+ 
+    favoriteMovies.push(movie);
+  },
+ 
+  deleteMovie(id) {
+    // cara boros menghapus film dengan meng-copy film yang ada
+    // kecuali film dengan id == id
+    favoriteMovies = favoriteMovies.filter((movie) => movie.id != id);
+  },
+};
+ 
+describe('Favorite Movie Array Contract Test Implementation', () => {
+  afterEach(() => favoriteMovies = []);
+ 
+  itActsAsFavoriteMovieModel(FavoriteMovieArray);
+});
